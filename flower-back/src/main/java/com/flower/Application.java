fcb40034c47e57abf074f48b104964a64e274912
yumaoying <18310235049>
2018-05-04 20:42:31 +0800

    删除多余模块，优化页面

--- flower-back/src/main/java/com/flower/Application.java
+++ flower-back/src/main/java/com/flower/Application.java
@@ -18,15 +18,17 @@ public class Application {
 
     /***
      *Tomcat large file upload connection reset
+     * Tomcat数据连接池配置
      * tomcatEmbedded这段代码是为了解决，上传文件大于10M出现连接重置的问题。
      * 此异常内容GlobalException也捕获不到。
-     * @return
+     * @return tomcat配置
      */
     @Bean
     public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
         TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
         tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
             Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+            //上传文件大于10M出现连接重置的问题
             protocol.setMaxSwallowSize(-1);
             //设置最大连接数
             protocol.setMaxConnections(2000);
